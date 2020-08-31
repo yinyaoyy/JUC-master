84d8ea3f54d12cd40c5d8a884d09a2e94ee0c98d
yinyao_yy <yinyao_yy@163.com>
2020-08-31 10:14:36 +0800

    Initial commit

---
+++ src/main/java/com/mashibing/juc/c_020/T12_TestExchanger.java
@@ -0,0 +1,38 @@
+package com.mashibing.juc.c_020;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * 只能是两个线程之间进行数据交换，调用exchange（）方法会进行阻塞。
+ */
+public class T12_TestExchanger {
+
+    static Exchanger<String> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            String s = "T1";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t1").start();
+
+
+        new Thread(()->{
+            String s = "T2";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t2").start();
+
+
+    }
+}
